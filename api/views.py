1c2ab855d44c6bbdb608a973481fb4241c33bb43
ZARIFMAC <uzbzarif@gmail.com>
2023-03-14 10:10:03 +0500

    get student

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Student,Contact
+from django.http import JsonResponse,HttpRequest
 
-# Create your views here.
+# Create view for Student
+def get_student(request:HttpRequest)->JsonResponse:
+    """
+    Get all student
+    """
+    pass
+
+
