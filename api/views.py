3f7e7eeb59ce6fc3e393bab40edd2568612408b6
azimabdurasulov <azimjon20050604@gmail.com>
2023-03-14 11:14:45 +0500

    inti

--- api/views.py
+++ api/views.py
@@ -7,6 +7,22 @@ def get_student(request:HttpRequest)->JsonResponse:
     """
     Get all student
     """
-    pass
+    students=Student.objects.all()
+    contacts=Contact.objects.all()
+    result={}
+    for student in students:
+        result[student.id]={
+            'first_name': student.first_name,
+            'last_name':student.last_name,
+            'number':student.contact.phone,
+            'address':student.contact.address
+            }
+        
+    # for contact in contacts:
+    #     result[student.id]['number']=contact.phone
+    #     result[student.id]['adress']=contact.address
+        
+    return JsonResponse(result)
+
 
 
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import get_student
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/', get_student)
 ]
